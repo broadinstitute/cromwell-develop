500b0763ce00b36aad690e38d895137bcec14db3
Henry Ferrara <hjferrara@bynara.com>
2017-11-28 14:11:46 -0500

    removed debugging statements

--- jenkins/cromiam-release.sh
+++ jenkins/cromiam-release.sh
@@ -9,13 +9,14 @@ then
   for image in ${DOCKER_IMAGES}
   do
      echo "Creating ${ENV} tag for ${image}..."
-     echo docker pull broadinstitute/${image}:${DOCKER_TAG}
-     echo docker tag broadinstitute/${image}:${DOCKER_TAG} broadinstitute/${image}:${ENV}
-     echo docker push broadinstitute/${image}:${ENV}
-     echo docker rmi broadinstitute/${image}:${ENV} broadinstitute/${image}:${DOCKER_TAG}
+     docker pull broadinstitute/${image}:${DOCKER_TAG}
+     docker tag broadinstitute/${image}:${DOCKER_TAG} broadinstitute/${image}:${ENV}
+     docker push broadinstitute/${image}:${ENV}
+     docker rmi broadinstitute/${image}:${ENV} broadinstitute/${image}:${DOCKER_TAG}
   done
   echo "ENV=${ENV}" >> cromwell.properties
   echo "PROJECT=caas" >> cromwell.properties
+  echo "BRANCH=hf_cleanup" >> cromwell.properties
 
 else
   echo; echo "DOCKER_TAG was not set! Nothing to do!  Exiting!"; echo
