13f2cf556bf6a7ae98fe39da09764d34a41f2a8d
Henry Ferrara <ferrara@broadinstitute.org>
2019-11-18 13:48:27 -0500

    remove old trusttore renders and mounts

--- configs/caas/manifest.rb
+++ configs/caas/manifest.rb
@@ -10,12 +10,5 @@ copy_secret_from_path "secret/common/ca-bundle.crt", "chain"
 copy_secret_from_path "secret/dsde/caas/#{$env}/common/server.key"
 copy_secret_from_path "secret/dsde/caas/#{$env}/common/server.crt"
 
-mysql = "secret/dsde/caas/#{$env}/cromwell/cromwell-mysql"
-copy_secret_from_path mysql, field = "keystore", output_file_name = "keystore.b64"
-base64decode "keystore.b64", "mysql01.jks"
-
-copy_secret_from_path "secret/dsde/caas/#{$env}/cromwell/cromwell-mysql-trust", field = "value", output_file_name = "mysql_trust.jks.b64"
-base64decode "mysql_trust.jks.b64", "mysql_trust.jks"
-
 copy_file "stackdriver.conf"
 copy_file "cromwell_log.conf"
